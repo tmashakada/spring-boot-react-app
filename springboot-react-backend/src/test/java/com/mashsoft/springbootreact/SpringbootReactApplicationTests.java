e23c6e4b9d4fc6a465fe2157aeb187a700d7436f
Tafadzwa Mashakada <tmashakada10@gmail.com>
2023-10-12 18:53:16 +0200

    initial Backend commit

---
+++ springboot-react-backend/src/test/java/com/mashsoft/springbootreact/SpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashsoft.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
