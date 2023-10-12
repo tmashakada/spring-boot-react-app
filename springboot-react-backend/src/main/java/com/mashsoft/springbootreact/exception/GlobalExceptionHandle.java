16b6aab21687adc437d532e2dc198262fc0263f3
Tafadzwa Mashakada <tmashakada10@gmail.com>
2023-10-12 21:26:38 +0200

    Backend Code implementation

--- springboot-react-backend/src/main/java/com/mashsoft/springbootreact/exception/GlobalExceptionHandle.java
+++ springboot-react-backend/src/main/java/com/mashsoft/springbootreact/exception/GlobalExceptionHandle.java
@@ -1,7 +1,7 @@
 package com.mashsoft.springbootreact.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
